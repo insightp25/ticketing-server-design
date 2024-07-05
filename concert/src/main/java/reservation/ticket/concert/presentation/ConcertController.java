99330c1403d63caa2cc3dd122df34888e8beb9df
JO <71416000+insightp25@users.noreply.github.com>
2024-07-05 20:09:43 +0900

    Mock API 추가

--- concert/src/main/java/reservation/ticket/concert/presentation/ConcertController.java
+++ concert/src/main/java/reservation/ticket/concert/presentation/ConcertController.java
@@ -1,2 +1,34 @@
-package reservation.ticket.concert.presentation;public class ConcertController {
+package reservation.ticket.concert.presentation;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 대기열 토큰에 userId와 concertId
+ */
+@RestController
+@RequestMapping("/concerts")
+public class ConcertController {
+
+    @GetMapping("/{id}")
+    public String concert(
+            @PathVariable Long id
+    ) {
+        return "concertTitle";
+    }
+
+    @GetMapping("/{id}/reservations")
+    public String reservation(
+            @PathVariable Long id,
+            @RequestParam Long user_id
+    ) {
+        return "ticketTitle, ticketPrice, List<Seat>, lastCompletedWaitId";
+    }
+
+    @PostMapping("/{id}/payments")
+    public String payment(
+            @PathVariable String id,
+            @RequestParam String user_id
+    ) {
+        return "reservationId, concertTitle";
+    }
 }
